475f1189b388de13b08c37340ffdd5a851455a66
Hamish <leithhamish@gmail.com>
2020-11-09 14:46:38 +0000

    a

--- js/app-ui.js
+++ js/app-ui.js
@@ -2,3 +2,13 @@ function setColor (color) {
     chosenColor = color;
 }
 
+
+var text_1 = document.getElementById('text_1');
+
+text_1.addEventListener('keydown', setText_1, false);
+
+function setText_1 () {
+    text_1 = document.getElementById(text_1).innerHTML;
+    console.log(text_1);
+};
+
---
+++ js/canvas-text.js
@@ -0,0 +1,4 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+ctx.font = "40px Cookie";
+ctx.fillText("Nathalie Eichler", 20, 50); 
